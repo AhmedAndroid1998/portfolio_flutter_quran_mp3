22b29594bbdccd5ce3a0542ab95142db7623635c
Ahmed98 <ahmeddigger1998@gmail.com>
2025-10-06 08:29:09 +0300

    initial setup: changed ApplicationId, create folders for  clean code structure, and set Arabic RTL layout as the default

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuranAudioLibraryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QuranAudioLibraryApp extends StatelessWidget {
+  const QuranAudioLibraryApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MyHomePage()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Quran_MP3',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+
+        /// the app will be Arabic-first.
+        supportedLocales: const [Locale('ar')],
+        locale: const Locale('ar'),
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: const Text(
+          'القرآن الكريم - المكتبة الصوتية',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: Text("Hello")
+      body: const Center(
+        child: Text(
+          'Welcome to Quran Audio',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
 }
