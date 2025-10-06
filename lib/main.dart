dee29388b07247853a51116f8fe0fab3edbfb6f2
Ahmed98 <ahmeddigger1998@gmail.com>
2025-10-06 11:29:51 +0300

    implement the search & filtering list feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:quran_mp3/data/reciters_list.dart';
-import 'package:quran_mp3/data/surah_list.dart';
+
+import 'screens/home_page.dart';
 
 void main() {
   runApp(const QuranAudioLibraryApp());
@@ -29,96 +29,6 @@ class QuranAudioLibraryApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'القرآن الكريم - المكتبة الصوتية',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            Expanded(child: _buildReciterSection()),
-            SizedBox(
-              width: 10,
-            ),
-            Expanded(child: _buildSurahSection()),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildReciterSection() {
-    return Card(
-      elevation: 10,
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(icon: Icon(Icons.search)),
-          ),
-          Expanded(
-            child: ListView.separated(
-                itemCount: recitersList.length,
-                separatorBuilder: (context, index) => const Divider(
-                      height: 1,
-                      color: Colors.grey,
-                    ),
-                itemBuilder: (context, index) {
-                  final reciter = recitersList[index];
-                  return ListTile(
-                    title: Text(
-                      reciter.name,
-                      textDirection: TextDirection.rtl,
-                    ),
-                  );
-                }),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _buildSurahSection() {
-    return Card(
-      elevation: 10,
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(icon: Icon(Icons.search)),
-          ),
-          Expanded(
-            child: ListView.separated(
-                itemCount: surahList.length,
-                separatorBuilder: (context, index) => const Divider(
-                      height: 1,
-                      color: Colors.grey,
-                    ),
-                itemBuilder: (context, index) {
-                  final surah = surahList[index];
-                  return ListTile(
-                    title: Text(
-                      '${index + 1}\t\t\t${surah.name}',
-                      textDirection: TextDirection.rtl,
-                    ),
-                  );
-                }),
-          )
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:quran_mp3/data/reciters_list.dart';
+import 'package:quran_mp3/data/surah_list.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List filteredReciterList = recitersList;
+  List filteredSurahList = surahList;
+
+  void _filterReciter(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        filteredReciterList = recitersList;
+      } else {
+        filteredReciterList = recitersList
+            .where((surah) =>
+                    surah.name.contains(query) ||
+                    surah.name
+                        .toLowerCase()
+                        .contains(query.toLowerCase()) //safety for E search,
+                )
+            .toList();
+      }
+    });
+  }
+
+  void _filterSurah(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        filteredSurahList = surahList;
+      } else {
+        filteredSurahList = surahList
+            .where((surah) =>
+                    surah.name.contains(query) ||
+                    surah.name
+                        .toLowerCase()
+                        .contains(query.toLowerCase()) //safety for E search,
+                )
+            .toList();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'القرآن الكريم - المكتبة الصوتية',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            Expanded(child: _buildReciterSection()),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(child: _buildSurahSection()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildReciterSection() {
+    return Card(
+      elevation: 10,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: _filterReciter, // 🔄 Filter live
+          ),
+          Expanded(
+            child: ListView.separated(
+                itemCount: filteredReciterList.length,
+                separatorBuilder: (context, index) => const Divider(
+                      height: 1,
+                      color: Colors.grey,
+                    ),
+                itemBuilder: (context, index) {
+                  final reciter = filteredReciterList[index];
+                  return ListTile(
+                    title: Text(
+                      reciter.name,
+                      textDirection: TextDirection.rtl,
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSurahSection() {
+    return Card(
+      elevation: 10,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: _filterSurah,
+          ),
+          Expanded(
+            child: ListView.separated(
+                itemCount: filteredSurahList.length,
+                separatorBuilder: (context, index) => const Divider(
+                      height: 1,
+                      color: Colors.grey,
+                    ),
+                itemBuilder: (context, index) {
+                  final surah = filteredSurahList[index];
+                  return ListTile(
+                    title: Text(
+                      '${index + 1}\t\t\t${surah.name}',
+                      textDirection: TextDirection.rtl,
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
