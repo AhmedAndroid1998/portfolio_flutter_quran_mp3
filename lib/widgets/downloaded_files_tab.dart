4344dff0af812e5db97281dbea82c4ac7dd6faf9
Ahmed98 <ahmeddigger1998@gmail.com>
2025-10-23 09:39:07 +0300

    design the Downloads layout using DefaultTabController with TabBar & TabBarView

---
+++ lib/widgets/downloaded_files_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DownloadedFilesTab extends StatelessWidget {
+  const DownloadedFilesTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
--- lib/widgets/downloads-section.dart
+++ lib/widgets/downloads-section.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:quran_mp3/widgets/downloaded_files_tab.dart';
+import 'package:quran_mp3/widgets/downloading_files_tab.dart';
 
 class DownloadsSectionWidget extends StatelessWidget {
   const DownloadsSectionWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.yellow,
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          Directionality(
+            textDirection: TextDirection.rtl,
+            child: Container(
+              color: Colors.blueAccent,
+              child: const TabBar(
+                indicatorColor: Colors.lightBlueAccent,
+                indicatorWeight: 3,
+                tabs: [
+                  Tab(
+                    text: 'الملفات المحملة',
+                  ),
+                  Tab(
+                    text: 'جاري التحميل...',
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: TabBarView(
+              children: [DownloadedFilesTab(), DownloadingFilesTab()],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
